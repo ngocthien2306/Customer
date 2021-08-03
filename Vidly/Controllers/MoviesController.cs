1408398d64f94b12aa0db6c3651a4a81cdff0a90
ngocthien2306 <75513398+ngocthien2306@users.noreply.github.com>
2021-08-03 22:45:29 +0700

    show movie to table

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        private ApplicationDbContext _context;
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+        public ActionResult Index()
+        {
+            var movie = _context.Movies.Include(m => m.Genres).ToList();
+            return View(movie);
+        }
+    }
+}
